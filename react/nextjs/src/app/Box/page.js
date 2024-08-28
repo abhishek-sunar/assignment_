f728e11832b55548a7979163e5e6d4570066e8a0
abhishek-sunar <abhisheksunar4526@gmail.com>
2024-08-28 20:54:52 +0545

    added code

--- react/nextjs/src/app/Box/page.js
+++ react/nextjs/src/app/Box/page.js
@@ -1,28 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+'use client'
+import { changeBackgroundColor, changeShape ,moveBottom,decreaseWidth,increaseWidth,moveLeft,moveRight, moveUp} from '@/redux/reducerSlice/boxslice'
+import { Button } from '@nextui-org/react'
+import React from 'react'
+import { FaArrowDown, FaArrowLeft, FaArrowRight, FaArrowUp } from 'react-icons/fa'
+import { useDispatch, useSelector } from 'react-redux'
 
-export const boxSlice = createSlice({
-  name: 'box',
-  initialState: {
-    width: 40,
-    height: 40,
-    backgroundColor: 'red',
-    borderRadius: 0,
-    padding:20,
-    margin:20
-  },
-  reducers: {
-    changeToCircle: (state, action) => {
-       state.height=state.width
-       state.borderRadius= '50%'
-    },
-    increaseWidth: (state, action) => {
-      state.width++
-   },
-    
-  }
-})
+const box = () => {
+    const dispatch = useDispatch()
+    const {padding, width,isCircle,marginLeft,marginTop, height,margin,borderRadius,backgroundColor} = useSelector((state) => state.box)
+    const area = borderRadius ? Math.PI * (width/2)**2 : width*height
+    const handleChange =(event)=> {
+      dispatch(changeBackgroundColor(event.target.value))
+    }
+  return (
+    <div>
+      <div className='m-10'>Area is {area}</div>
+        <div className='p-4 m-4 w-2'
+          onClick={()=> dispatch(changeShape())}
+          style={{
+            position: 'absolute',
+            marginLeft: marginLeft,
+            marginTop: marginTop,
+            backgroundColor:backgroundColor, width:width, margin:margin, height:height, borderRadius:  borderRadius}}>
+        </div>
+        <input className='m-4 p-4 w-20%' onChange={handleChange} placeholder="Property:value"/>
+        <Button onClick={()=> dispatch(decreaseWidth())}>-</Button>
+        <Button onClick={()=> dispatch(increaseWidth())}>+</Button>
+        <Button onClick={()=>dispatch(moveLeft())}><FaArrowLeft/></Button>
+        <Button onClick={()=> dispatch(moveRight())}><FaArrowRight/></Button>
+        <Button onClick={()=> dispatch(moveUp())}><FaArrowUp/></Button>
+        <Button  onClick={()=> dispatch(moveBottom())}><FaArrowDown/></Button>
+    </div>
+  )
+}
 
-
-export const { changeToCircle,increaseWidth } = boxSlice.actions
-
-export default boxSlice.reducer
+export default box
--- react/nextjs/src/redux/reducerSlice/boxslice.js
+++ react/nextjs/src/redux/reducerSlice/boxslice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit'
-
 export const boxSlice = createSlice({
   name: 'box',
   initialState: {
@@ -8,21 +7,46 @@ export const boxSlice = createSlice({
     backgroundColor: 'red',
     borderRadius: 0,
     padding:20,
-    margin:20
+    margin:20,
+    marginLeft: 50,
+    marginTop: 50,
+    isCircle: false
   },
   reducers: {
-    changeToCircle: (state, action) => {
-       state.height=state.width
-       state.borderRadius= '50%'
+    changeShape: (state, action) => {
+      if(state.borderRadius == '50%'){
+        state.borderRadius=0
+      }
+      else{
+        state.height=state.width
+        state.borderRadius= '50%'
+      }
     },
     increaseWidth: (state, action) => {
-      state.width++
-   },
-    
+    state.width++
+    },
+    decreaseWidth:(state,action) => {
+    state.width--
+    },
+    changeBackgroundColor: (state, action) => {
+    state.backgroundColor = action.payload
+    },
+    moveRight: (state, action) => {
+    state.marginLeft= state.marginLeft+20
+    },
+    moveLeft: (state, action) => {
+    state.marginLeft= state.marginLeft-20
+    },
+    moveBottom: (state, action) => {
+    state.marginTop =  state.marginTop +20
+    },
+    moveUp: (state, action) => {
+    state.marginTop =  state.marginTop -20
+    },
   }
 })
 
 
-export const { changeToCircle,increaseWidth } = boxSlice.actions
+export const { changeShape,moveUp,increaseWidth,decreaseWidth ,moveBottom,changeBackgroundColor, moveRight,moveLeft } = boxSlice.actions
 
 export default boxSlice.reducer
